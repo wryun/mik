f852319bcc8464f98cda4f3325d0dbb0d2d05b4b
James Haggerty <jamesh@groklearning.com>
2020-04-22 22:46:55 +1000

    Minimal get it to compile on 0.6
    Doesn't actually make sense/work.
    
    Lots of std lib changes. Probably should stop using ArrayListSentinel blah.

--- src/main.zig
+++ src/main.zig
@@ -11,25 +11,25 @@ const CommandError = error{
 
 const Command = struct {
     allocator: *std.mem.Allocator,
-    name: std.Buffer,
-    help: std.Buffer,
-    args: std.Buffer,
+    name: std.ArrayListSentineled(u8, 0),
+    help: std.ArrayListSentineled(u8, 0),
+    args: std.ArrayListSentineled(u8, 0),
 
     fn init(allocator: *std.mem.Allocator) !Command {
         return Command{
             .allocator = allocator,
-            .name = try std.Buffer.initSize(allocator, 0),
-            .help = try std.Buffer.initSize(allocator, 0),
-            .args = try std.Buffer.initSize(allocator, 0),
+            .name = try std.ArrayListSentineled(u8, 0).initSize(allocator, 0),
+            .help = try std.ArrayListSentineled(u8, 0).initSize(allocator, 0),
+            .args = try std.ArrayListSentineled(u8, 0).initSize(allocator, 0),
         };
     }
 
     fn clone(self: *Command) !Command {
         return Command{
             .allocator = self.allocator,
-            .name = try std.Buffer.init(self.allocator, self.name.toSliceConst()),
-            .help = try std.Buffer.init(self.allocator, self.help.toSliceConst()),
-            .args = try std.Buffer.init(self.allocator, self.args.toSliceConst()),
+            .name = try std.ArrayListSentineled(u8, 0).init(self.allocator, self.name.list.items),
+            .help = try std.ArrayListSentineled(u8, 0).init(self.allocator, self.help.list.items),
+            .args = try std.ArrayListSentineled(u8, 0).init(self.allocator, self.args.list.items),
         };
     }
 
@@ -39,7 +39,7 @@ const Command = struct {
         try self.args.resize(0);
     }
 
-    fn _setField(self: *Command, buf: *std.Buffer, value: []const u8) !void {
+    fn _setField(self: *Command, buf: *std.ArrayListSentineled(u8, 0), value: []const u8) !void {
         if (buf.len() != 0) {
             return CommandError.FieldDefinedTwice;
         }
@@ -53,54 +53,62 @@ const Command = struct {
         } else if (std.mem.eql(u8, key, "args")) {
             try self._setField(&self.args, value);
         } else {
-            std.debug.warn("{}\n", key);
+            std.debug.warn("{}\n", .{key});
             return CommandError.NoSuchField;
         }
     }
 
     fn showHelp(self: *const Command) void {
-        std.debug.warn("  {s10}  {}\n", self.name.toSliceConst(), self.help.toSliceConst());
+        //std.debug.warn("  {s10}  {}\n", .{self.name.list.items, self.help.list.items});
     }
 
     fn showFullHelp(self: *const Command) void {
-        std.debug.warn("mik {} {}", self.name.toSliceConst(), self.args.toSliceConst());
-        std.debug.warn("{}\n", self.help.toSliceConst());
+        std.debug.warn("mik {} {}", .{ self.name.list.items, self.args.list.items });
+        std.debug.warn("{}\n", .{self.help.list.items});
     }
 
-    fn parseArgString(self: *const Command) !std.hash_map.AutoHashMap([]const u8, Argument) {
-        const args = std.hash_map.AutoHashMap([]const u8, []const []const u8).init(self.allocator);
-        const positionals = 
-        var it = mem.tokenize(self.args, " \t\r\n");
+    //fn parseArgString(self: *const Command) !std.hash_map.AutoHashMap([]const u8, Argument) {
+    fn parseArgString(self: *const Command) std.hash_map.StringHashMap([]const []const u8) {
+        //const args = std.hash_map.AutoHashMap([]const u8, []const []const u8).init(self.allocator);
+        const args = std.hash_map.StringHashMap([]const []const u8).init(self.allocator);
+        //const positionals =
+        var it = std.mem.tokenize(self.args.list.items, " \t\r\n");
         while (it.next()) |token| {
-            if (mem.startsWith(u8, token, "-")) {
-            } else if (mem.startsWith(u8, token, "<") and mem.endsWith(u8, token, ">")) {
-                token[1..-1]
+            if (std.mem.startsWith(u8, token, "-")) {} else if (std.mem.startsWith(u8, token, "<") and std.mem.endsWith(u8, token, ">")) {
+                //token[1..-1]
             }
         }
 
+        return args;
     }
 
     fn exec(self: *const Command, tmp_file: []const u8) !void {
         const argsMap = self.parseArgString();
         var env = try processArgs(self.allocator, self, &std.process.args());
         // TODO pass remaining std.process.args
-        try std.os.execve(self.allocator, [_][]const u8{ "bash", tmp_file }, &env);
+        //try std.os.execveZ(self.allocator, [_][]const u8{ "bash", tmp_file }, &env);
+        //try std.os.execveZ("bash", .{ try std.mem.allocSentinel(self.allocator, u8, tmp_file.len, null) }, &env);
+        return std.os.execvpe(self.allocator, &[_][]const u8{ "bash", tmp_file }, &env);
     }
 };
 
-fn processFile(allocator: *std.mem.Allocator, input: *std.io.InStream(std.os.ReadError), output: *std.io.OutStream(std.os.WriteError), command_name: []const u8) !?Command {
+fn processFile(allocator: *std.mem.Allocator, input: std.fs.File.InStream, output: std.fs.File.OutStream, command_name: []const u8) !?Command {
     const help_mode = std.mem.eql(u8, command_name, "help");
-    var buf = try std.Buffer.initSize(allocator, 1000);
     var current_command = try Command.init(allocator);
 
     // for amusement's sake (and to use a stupidly small amount of memory), we
     // don't build a hashmap of all the commands but instead return only
     // that referenced by command_name.
     var final_command: ?Command = null;
-    // TODO smaller static buffer, only read start of line?
-    while (std.io.readLineFrom(input, &buf)) |line| {
-        try output.write(line);
-        try output.write("\n");
+
+    // Consider buffering situation.
+    const in_stream = std.io.bufferedInStream(input).inStream();
+    const out_stream = std.io.bufferedOutStream(output).outStream();
+    var line_buf: [512]u8 = undefined;
+    while (in_stream.readUntilDelimiterOrEof(&line_buf, '\n')) |l| {
+        const line = l.?;
+        _ = try output.write(line);
+        _ = try output.write("\n");
 
         if (std.mem.startsWith(u8, line, "mik_") and std.mem.endsWith(u8, line, "() {")) {
             try current_command.name.replaceContents(line[4..std.mem.indexOfScalar(u8, line, '(').?]);
@@ -117,21 +125,19 @@ fn processFile(allocator: *std.mem.Allocator, input: *std.io.InStream(std.os.Rea
             const value = line[colonIndex + 1 ..];
             if (!std.mem.startsWith(u8, field, "global_")) {
                 current_command.setField(field, value) catch |err| {
-                    std.debug.warn("{} multiply defined as {}", field, value);
+                    std.debug.warn("{} multiply defined as {}", .{ field, value });
                     return err;
                 };
             }
         }
-
-        try buf.resize(0);
     } else |err| {}
 
     if (final_command) |command| {
-        try output.write("mik_");
-        try output.write(command.name.toSliceConst());
-        try output.write(" \"$@\"\n");
+        _ = try output.write("mik_");
+        _ = try output.write(command.name.list.items);
+        _ = try output.write(" \"$@\"\n");
     } else if (!help_mode) {
-        std.debug.warn("missing '{}' - try mik help\n", command_name);
+        std.debug.warn("missing '{}' - try mik help\n", .{command_name});
     }
 
     return final_command;
@@ -149,34 +155,31 @@ fn processArgs(allocator: *std.mem.Allocator, command: *const Command, args_it: 
 }
 
 fn generate_scaffold() !void {
-    const file = std.fs.File.openWriteNoClobber(MIKFILE, std.fs.File.default_mode) catch |err| switch (err) {
+    const file = std.fs.cwd().createFile(MIKFILE, .{ .exclusive = true }) catch |err| switch (err) {
         std.fs.File.OpenError.PathAlreadyExists => {
-            std.debug.warn("Cowardly refusing to overwrite existing {}\n", MIKFILE);
+            std.debug.warn("Cowardly refusing to overwrite existing {}\n", .{MIKFILE});
             return;
         },
         else => return err,
     };
     defer file.close();
-    try file.write(MIKFILE_SCAFFOLD);
+    _ = try file.write(MIKFILE_SCAFFOLD);
 }
 
 pub fn main() anyerror!void {
-    var direct_allocator = std.heap.DirectAllocator.init();
-    defer direct_allocator.deinit();
-
     // using an arena allocator means we don't have to worry about deinit as we go.
     // tbh, it's all kinda a waste of time anyway, as the arena.deinit
     // will never run on a correct execution (since we're exec-ing ...).
-    var arena = std.heap.ArenaAllocator.init(&direct_allocator.allocator);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
     const allocator = &arena.allocator;
 
     // TODO generate name randomly... or at least process id suffix
-    const tmp_file = "/tmp/Mikfile.bash";
-    var output = try std.fs.File.openWrite(tmp_file);
-    try output.write("rm " ++ tmp_file ++ "\n");
-    errdefer std.fs.deleteFile(tmp_file) catch {};
+    const tmp_file = "/tmp/Mikfile.sh";
+    var output = try std.fs.cwd().createFile(tmp_file, .{ .exclusive = true });
+    _ = try output.write("rm " ++ tmp_file ++ "\n");
+    errdefer std.fs.cwd().deleteFile(tmp_file) catch {};
     defer output.close();
 
     var args_it = std.process.args();
@@ -199,10 +202,10 @@ pub fn main() anyerror!void {
         }
     }
 
-    var input = try std.fs.File.openRead(MIKFILE);
+    var input = try std.fs.cwd().openFile(MIKFILE, .{});
     defer input.close();
 
-    var command = try processFile(allocator, &input.inStream().stream, &output.outStream().stream, command_name);
+    var command = try processFile(allocator, input.inStream(), output.outStream(), command_name);
 
     if (command) |command_val| {
         if (command_help) {
